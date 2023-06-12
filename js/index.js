09c2e82024c326d2d5397d3a1cc255d31cef893c
home912 <mr.vladbutorin@mail.ru>
2023-06-12 18:03:46 +0700

    Change theme

--- js/index.js
+++ js/index.js
@@ -42,3 +42,11 @@ const swiper = new Swiper('.swiper', {
     },
 
 });
+
+// Выбираем кнопку
+const btn = document.querySelector('.slider');
+// Отслеживаем щелчок по кнопке
+btn.addEventListener('click', function () {
+    // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
+    document.body.classList.toggle('dark-theme');
+})
